d26729404fcc99386f8878fdd5ccda86859ee774
NILOY ROY <121684663+xanon-oc@users.noreply.github.com>
2023-05-02 11:31:33 +0600

    selected by id

--- index.js
+++ index.js
@@ -15,6 +15,17 @@ app.get("/all", (req, res) => {
   res.send(data);
 });
 
+// app.get("/all/recipes", (req, res) => {
+//   const id = req.params.chef_id;
+//   const selectedNews = data.find((n) => n.chef_id === id);
+//   res.send(selectedNews);
+// });
+app.get("/all/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = data.find((n) => n.id === id);
+  res.send(selectedNews);
+});
+
 app.listen(port, () => {
   console.log(`Dragon API is running on port : ${port}`);
 });
